451175896f8ccba3372072e203147b7b655236a1
Abhinav Batra <abhinavbatra2303@gmail.com>
2024-01-20 16:33:21 -0500

    test endpoint calling a query

--- backend/__init__.py
+++ backend/__init__.py
@@ -1,13 +1,22 @@
 from flask import Flask, jsonify, make_response
 from flask_cors import CORS
+import psycopg2
+from dotenv import load_dotenv
+import os
+from supabase import create_client, Client
 
 
-app = Flask(__name__)
 
 def create_app(test_config=None):
-    
     app = Flask(__name__)
-    
+
+    load_dotenv()
+
+    # PostgreSQL connection parameters
+    url: str = os.environ.get("SUPABASE_URL")
+    key: str = os.environ.get("SUPABASE_KEY")
+    supabase = create_client(url, key)
+        
     # setup
     CORS(app, resources={r'/*': {'origins': '*'}})
 
@@ -16,6 +25,17 @@ def create_app(test_config=None):
     @app.route('/hello')
     def hello():
         return make_response('hello world', 200)
+    
+    # Test query
+    @app.route('/test_query')
+    def test_query():
+        # Replace 'restaurant' with the actual table name
+        table_name = 'restaurant'
+        
+        # Use 'from' method to interact with the table
+        data, count = supabase.table(table_name).select('*').execute()
+        
+        return make_response(jsonify(data), 200)
 
     
     
